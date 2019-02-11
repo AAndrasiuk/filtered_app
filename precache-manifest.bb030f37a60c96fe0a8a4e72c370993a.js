3d24e446c76e7b57e629164a6a11c1bf08e1b459
Aliaksandr <a.andrasiuk@gmail.com>
2019-02-11 21:21:54 +0100

    Updates

---
+++ precache-manifest.bb030f37a60c96fe0a8a4e72c370993a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8df285ed8ba6d20e034a99479a88856f",
+    "url": "/AAndrasiuk.github.io/filtered_app/static/media/filter_example_1.8df285ed.jpeg"
+  },
+  {
+    "revision": "b54660abfbf7abcd1710",
+    "url": "/AAndrasiuk.github.io/filtered_app/static/js/runtime~main.b54660ab.js"
+  },
+  {
+    "revision": "4a6e6e9f12575ffa7a4d",
+    "url": "/AAndrasiuk.github.io/filtered_app/static/js/main.4a6e6e9f.chunk.js"
+  },
+  {
+    "revision": "7bb9cd69eb0a9d5acdaa",
+    "url": "/AAndrasiuk.github.io/filtered_app/static/js/1.7bb9cd69.chunk.js"
+  },
+  {
+    "revision": "4a6e6e9f12575ffa7a4d",
+    "url": "/AAndrasiuk.github.io/filtered_app/static/css/main.e6708913.chunk.css"
+  },
+  {
+    "revision": "603e0b8d4c5be8324723e8429bfbde16",
+    "url": "/AAndrasiuk.github.io/filtered_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AAndrasiuk.github.io/filtered_app/precache-manifest.09f19b814d247317e08ac204f28d3da9.js"
+  "/AAndrasiuk.github.io/filtered_app/precache-manifest.bb030f37a60c96fe0a8a4e72c370993a.js"
 );
 
 workbox.clientsClaim();
